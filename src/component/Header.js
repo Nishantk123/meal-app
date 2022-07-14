9d24263ac91143f074a1932d67236d1a5722c100
Nishant kashyap <sannukashyap24@gmail.com>
2022-07-14 08:08:15 +0530

    header design change

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,7 @@ const Header = () =>{
 
     return(
         <div className="header px-2">
-            <div className="p-3">Quick Food</div>
+            <div className="p-3"><strong>Quick Food</strong></div>
             <div className="profile-container">
                 <img  className="profile-img" src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg" />
             </div>
